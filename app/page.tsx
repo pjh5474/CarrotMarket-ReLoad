f5db9f86edfacbfe1836049bdddeab27e4d40068
pjh5474 <warwarsn@gmail.com>
2024-04-28 18:52:27 +0900

    State Modifiers
    * / has

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2">
+      <div className="bg-green-100 w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none has-[.peer]:bg-white ring ring-transparent transition-shadow has-[:invalid]:ring-red-100">
         <input
-          className="w-full rounded-full py-3 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+          className="w-full rounded-full py-3 bg-gray-200 pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
           type="email"
           required
           placeholder="Email address"
@@ -11,7 +11,7 @@ export default function Home() {
         <span className="text-red-500 font-medium hidden peer-invalid:block">
           Email is required
         </span>
-        <button className="text-white py-2 rounded-full active:scale-90 outline-none transition-transform font-medium md:px-10 peer-valid:bg-gradient-to-tr from-cyan-500 via-green-200 to-purple-400 peer-invalid:bg-red-200  ">
+        <button className="text-white py-2 rounded-full active:scale-90 transition-transform font-medium md:px-10 peer-valid:bg-gradient-to-tr from-cyan-500 via-green-200 to-purple-400 peer-invalid:bg-red-200  ">
           Log in
         </button>
       </div>
