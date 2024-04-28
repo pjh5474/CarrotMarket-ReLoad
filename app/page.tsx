badb979f0e15966fd4632eabd711539a760a5b9d
pjh5474 <warwarsn@gmail.com>
2024-04-29 01:33:23 +0900

    Home Screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,27 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-3">
-        {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
-          <div
-            key={index}
-            className="flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 p-2.5 rounded-xl border-b-2 pb-5 last:border-green-300 first:border-red-300 group"
-          >
-            <div className="size-10 bg-blue-400 rounded-full" />
-            <div className="w-20 rounded-full empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-400 group-hover:text-red-500">
-              {person}
-            </div>
-            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full animate-bounce relative">
-              <span className="z-10">{index}</span>
-              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
-            </div>
-          </div>
-        ))}
-        <div className="group flex flex-col">
-          <input
-            className="bg-gray-100 w-full"
-            placeholder="Write your email"
-          />
-          <span className="hidden group-focus-within:block ">
-            Make sure it is a valid email
-          </span>
-          <button>Submit</button>
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link
+          href="/create-account"
+          className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link className="hover:underline underline-offset-2" href="/login">
+            로그인
+          </Link>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
