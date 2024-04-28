04ace6edb159f5d1b8c53c08a46c1a31a4d29083
pjh5474 <warwarsn@gmail.com>
2024-04-28 19:31:27 +0900

    Lists and Animations
    Empty State

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
       <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-3">
-        {["Nico", "Me", "You", "Yourself"].map((person, index) => (
+        {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
           <div
             key={index}
             className="flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 p-2.5 rounded-xl border-b-2 pb-5 last:border-green-300 first:border-red-300 "
           >
             <div className="size-10 bg-blue-400 rounded-full" />
-            <div className="w-20 rounded-full bg-gray-400 animate-pulse">
+            <div className="w-20 rounded-full empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-400">
               {person}
             </div>
             <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full animate-bounce relative">
