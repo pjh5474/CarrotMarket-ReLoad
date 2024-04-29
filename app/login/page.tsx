72918055164149c841a099ab5af3a03b4766908e
pjh5474 <warwarsn@gmail.com>
2024-04-29 18:26:34 +0900

    Route Handlers / API Route

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocilaLogin from "@/components/social-login";
 
 export default function LoginPage() {
+  const onClick = async () => {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username: "Nico",
+        password: "1234",
+      }),
+    });
+    console.log(await response.json());
+  };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -19,6 +31,10 @@ export default function LoginPage() {
         />
         <FormButton loading={false} text="Login" />
       </form>
+      <span onClick={onClick}>
+        <FormButton loading={false} text="Login" />
+      </span>
+
       <SocilaLogin />
     </div>
   );
