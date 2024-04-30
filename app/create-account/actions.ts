f3e906919427651de5cb9c7238c8169fe59c4dbc
pjh5474 <warwarsn@gmail.com>
2024-04-30 11:57:18 +0900

    Refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,33 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(8),
-  confirmPassword: z.string().min(8),
-});
+const checkUsername = (username: string) => !username.includes("potato");
+const checkPasswords = ({
+  password,
+  confirmPassword,
+}: {
+  password: string;
+  confirmPassword: string;
+}) => password === confirmPassword;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string!",
+        required_error: "Where is my username???",
+      })
+      .min(3, "Username 은 3 글자 이상, 10 글자 미만이어야 합니다.")
+      .max(10, "Username 은 3 글자 이상, 10 글자 미만이어야 합니다.")
+      .refine(checkUsername, "No potatoes allowed"),
+    email: z.string().email(),
+    password: z.string().min(8).max(20),
+    confirmPassword: z.string().min(8).max(20),
+  })
+  .refine(checkPasswords, {
+    message: "Password와 confromPassword가 일치하지 않습니다.",
+    path: ["confirmPassword"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
