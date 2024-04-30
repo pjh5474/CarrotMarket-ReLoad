e295919d1ad66c5067740ff8cb1293f8147a203b
pjh5474 <warwarsn@gmail.com>
2024-04-30 11:42:45 +0900

    Validation Errors

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,6 +1,13 @@
 "use server";
 import { z } from "zod";
 
+const formSchema = z.object({
+  username: z.string().min(3).max(10),
+  email: z.string().email(),
+  password: z.string().min(8),
+  confirmPassword: z.string().min(8),
+});
+
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
     username: formData.get("username"),
@@ -8,4 +15,8 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirmPassword: formData.get("confirmPassword"),
   };
+  const result = formSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  }
 }
